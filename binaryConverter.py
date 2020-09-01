9d7c9a24ba582f0a23cc7a7233c5115860f0fbcf
Collin Holmquist <collinholmquist@gmail.com>
2020-09-01 07:47:44 -0500

    Update binaryConverter.py

--- binaryConverter.py
+++ binaryConverter.py
@@ -1,1 +1,13 @@
+# binaryConverter.py
+# Name: 
+# Date:
+# Gist: This program should take a decimal number and convert it into binary.  Ex: 17 becomes 10001
 
+decimal_num = 3
+
+def to_binary():
+  pass
+
+
+
+to_binary() 
