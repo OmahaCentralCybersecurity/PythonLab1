7d484d8bcae14a50cca768a7aa47455a7e7c3ce6
Collin Holmquist <collinholmquist@gmail.com>
2020-08-31 22:26:03 -0500

    Update dice.py

--- dice.py
+++ dice.py
@@ -1,1 +1,25 @@
+## Dice.py
+## Name
+## Date
+##
+## Dice.py Part 1 will represent a user-input number of rolls of a normal 6-sided dice. 
+## Dice.py Part 2 will represent a dice with a user-input number of sides to be rolled. Will continue to roll until the highest number is rolled. 
+
+
+import random
+
+def dice1():
+  ## code for part 1 goes here
+  pass ## pass is used a placeholder for empty code. You can delete pass once you've written each function 
+
+
+def dice2():
+  ## code for part 2 goes here
+  pass
+
+
+# call each function to run
+dice1()
+# dice2()
+
 
